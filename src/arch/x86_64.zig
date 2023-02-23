8533e2a1362da67fa54a36c7a41888f48db61ac9
hi7 <hilger.zone@gmail.com>
2023-02-23 12:02:45 +0100

    adds x86_64 asm

---
+++ src/arch/x86_64.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+
+pub fn add(a: usize, b: usize) usize {
+    return asm volatile ("addq %rdi, %rax"
+        : [ret] "={rax}" (-> usize)
+        : [a] "{rax}" (a),
+          [b] "{rdi}" (b),
+        : "rcx", "r11"
+    );
+}
+
+test "add" {
+    try std.testing.expectEqual(@as(usize, 99999999), add(12345678, 87654321));
+}
--- test-all.sh
+++ test-all.sh
@@ -4,3 +4,4 @@ zig test src/boot.zig;
 zig test src/env.zig;
 zig test src/eval.zig;
 zig test src/print.zig;
+zig test src/arch/x86_64.zig;
